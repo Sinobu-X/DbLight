087223806177194bdc27a0a9e67265ee0034f341
sinobu <wjinp@hotmail.com>
2019-11-19 17:49:57 +0800

    sinobu

--- Src/netcore/DbLightTest/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System;
-
-//[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
-[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
